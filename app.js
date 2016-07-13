b63e92ddcb946a8fbe7893e0ba83dc5cc55ece34
谢震宇 <18819253799@163.com>
2016-07-13 22:01:17 +0800

    基本实现官网作业。

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 var socket = io();
+var name = prompt("请输入昵称", "guest");
+var keyboardPressCount = 0;
+
+socket.emit('add user', name);
+
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
+  var str = $('#m').val();
+  var strarr = [];
+  strarr = str.split("@");
+  socket.emit('chat message', strarr);
   $('#m').val('');
+  keyboardPressCount = 0;
   return false;
 });
-socket.on('message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+
+$('#m').keypress(function(){
+	if (keyboardPressCount == 0) {
+	    socket.emit('typing');
+	    keyboardPressCount++;
+	}
 });
-socket.on('connection', function(){
-  $('#messages').append($('<li>').text('a new user connected.'));
+
+socket.on('add user', function(data){
+  $('#messages').append($('<li>').text('用户 ' + data.username + ' 加入聊天。'));
 });
-socket.on('disconnect', function(){
-  $('#messages').append($('<li>').text('a user disconnected.'));
+
+socket.on('cancle typing', function(data){
+  $('.' + data.username).remove();  
+});
+
+socket.on('message', function(data){
+  $('#messages').append($('<li>').text(data.username + ': ' + data.message));
+});
+
+socket.on('delete user', function(data){
+  $('#messages').append($('<li>').text('用户 ' + data.username + '退出聊天。'));
+});
+
+socket.on('broadcast typing', function(data){
+	$('#messages').append($('<li>').text('用户' + data.username + '正在输入...'));
+	$('ul li:last-child').addClass(data.username);
 });
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {};
+
 app.use(express.static('./'));
 
 app.get('/', function(req, res){
@@ -10,12 +12,31 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-	    io.emit('message', msg);
+	socket.on('add user', function(name){
+		socket.username = name;
+		users[name] = socket;
+		io.emit('add user', {username: socket.username});
 	});
-	socket.broadcast.emit('connection');
+
 	socket.on('disconnect', function(){
-	    io.emit('disconnect');
+	    io.emit('delete user', {username: socket.username});
+	});
+
+	socket.on('chat message', function(data){
+		socket.broadcast.emit('cancle typing', {username: socket.username});
+		if (data.length > 1) {
+			for (var i = 1; i < data.length; i++) {
+				for (user in users)
+					if (users[user].username == data[i])
+						users[user].emit('message', {username: socket.username, message: data[0]})
+			}
+		} else {
+		    io.emit('message', {username: socket.username, message: data[0]});
+		}
+	});
+
+	socket.on('typing', function(){
+		socket.broadcast.emit('broadcast typing', {username: socket.username});
 	});
 });
 
